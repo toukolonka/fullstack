2582cd17740787450f22e47a796dee3895f29836
Your Name <you@example.com>
2020-11-16 11:40:45 +0200

    maiden_tiedot valmis

--- Round2/maiden_tiedot/src/App.js
+++ Round2/maiden_tiedot/src/App.js
@@ -1,31 +1,49 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
-import Countries from './components/Countries';
-import Filter from './components/Filter';
+import axios from "axios";
+import Country from "./components/Country";
+import Filter from "./components/Filter";
 
 const App = () => {
-
   const [countries, setCountries] = useState([]);
   const [checkName, setCheckName] = useState("");
 
   useEffect(() => {
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        setCountries(response.data)
-      })
-  }, [])
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
 
   const handleCheckChange = (event) => {
     setCheckName(event.target.value);
   };
 
-  const countriesToShow = countries.filter(country => country.name.toLowerCase().includes(checkName.toLowerCase()));
+  const countriesToShow = countries.filter((country) =>
+    country.name.toLowerCase().includes(checkName.toLowerCase())
+  );
+
+  let content;
+
+  if (countriesToShow.length > 10)
+    content = <p>Too many matches, specify another filter</p>;
+  else if (countriesToShow.length === 1) {
+    content = <Country country={countriesToShow[0]} />;
+  } else {
+    content = (
+      <ul>
+        {countriesToShow.map((country) => (
+          <li key={country.name}>
+            {country.name}
+            <button onClick={() => setCheckName(country.name)}>show</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
   return (
     <div>
       <Filter checkName={checkName} checkChange={handleCheckChange} />
-      <Countries countries={countriesToShow} />
+      {content}
     </div>
   );
 };
--- Round2/maiden_tiedot/src/components/Weather.js
+++ Round2/maiden_tiedot/src/components/Weather.js
@@ -1,27 +1,30 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 const Weather = ({ capital }) => {
-    
-    const [weather, setWeather] = useState("");
-    const api_key = process.env.REACT_APP_API_KEY
+  const [weather, setWeather] = useState("");
+  const api_key = process.env.REACT_APP_API_KEY;
 
-    useEffect(() => {
-        axios
-          .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`)
-          .then(response => {
-            setWeather(response.data.current)
-          })
-      })
+  useEffect(() => {
+    if (typeof capital === "string")
+      axios
+        .get(
+          `http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`
+        )
+        .then((response) => {
+          setWeather(response.data.current);
+        });
+  }, [capital, api_key]);
 
-    return ( 
-        <div>
-            <p>temperature: {weather.temperature} Celsius</p>
-            <img src={weather.weather_icons} alt="Icon" width="10%" height="10%" />
-            <p>wind: {weather.wind_speed} mph direction {weather.wind_dir}</p>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <p>temperature: {weather.temperature} Celsius</p>
+      <img src={weather.weather_icons} alt="Icon" width="10%" height="10%" />
+      <p>
+        wind: {weather.wind_speed} mph direction {weather.wind_dir}
+      </p>
+    </div>
+  );
+};
+
 export default Weather;
-  
